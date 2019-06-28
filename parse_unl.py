7d4d46e69e2f2cd011ecee4fe61593333dd2774f
crypticrabbit <the@rabbitkick.club>
2019-06-27 22:42:44 -0400

    Condense code and more verbose docstrings.

--- parse_unl.py
+++ parse_unl.py
@@ -1,5 +1,6 @@
 '''
-Retrieve and parse a published rippled UNL
+Retrieve and parse a published rippled UNL.
+Returns base58 encoded XRP Ledger keys.
 '''
 
 import json
@@ -22,14 +23,15 @@ def base58_encode(key):
 def parser(address):
     '''
     Download the UNL and base64 decode the blob.
+    Individual validation keys are then constructed from the decoded blob
+    payload and the double sha256 hashed checksums. Keys are then base58 encoded.
     '''
     validation_keys = []
     validators = json.loads(b64decode(
         json.loads(requests.get(address).content)['blob']))['validators']
     for i in validators:
         payload = "1C" + i['validation_public_key']
-        hash1 = sha256(bytearray.fromhex(payload)).hexdigest()
-        checksum = sha256(bytearray.fromhex(hash1)).hexdigest()[0:8]
-        validation_keys.append(base58_encode(int(payload + checksum, 16)).decode('utf-8'))
+        validation_keys.append(base58_encode(int(payload + sha256(bytearray.fromhex(
+            sha256(bytearray.fromhex(payload)).hexdigest())).hexdigest()[0:8], 16)).decode('utf-8'))
 
     return validation_keys
