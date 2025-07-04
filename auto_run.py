fae4fe503350a1c83957756cef41f10caa6ce993
JScottBranson <the@RabbitKick.Club>
2025-07-04 12:20:24 -0400

    Prevent writing a blank UNL if there is a http error.

--- auto_run.py
+++ auto_run.py
@@ -5,27 +5,45 @@ Write the results to a .json file.
 
 import time
 import json
+import logging
 
 from parse_unl import unl_parser
 
 UNL_URL = "https://vl.xahau.org"
-#UNL_URL = "https://vl.xrplf.org"
 #UNL_URL = "https://vl.ripple.com"
 
+TIME_PAUSE = 900 #Time in seconds to wait between queries.
 OUTPUT_FILE = "./unl_mappings.json"
-TIME_PAUSE = 120 #Time in seconds to wait between queries.
+LOG_FILE = "./unl_parser_auto.log"
+LOG_LEVEL = logging.INFO
+
+def start_log():
+    '''
+    Configure logging.
+    '''
+    logging.basicConfig(
+            filename=LOG_FILE,
+            level=LOG_LEVEL,
+            datefmt="%Y-%m-%d %H:%M:%S",
+            format='%(asctime)s %(levelname)s: %(module)s - %(funcName)s (%(lineno)d): %(message)s',
+            )
+    logging.info("Logging configured.")
 
 def write_out(unl):
-    if unl:
+    if unl['error'] is False and unl['http_code'] == 200:
         with open(OUTPUT_FILE, "w+") as output:
-            output.write(unl)
+            output.write(json.dumps(unl))
+        logging.info(f"Wrote UNL with: {unl['validator_count']} items.")
+    else:
+        logging.Warning(f"Error retrieving UNL: {unl}")
 
 def run():
+    start_log()
     n = 1
     while True:
         try:
             unl = unl_parser(UNL_URL)
-            write_out(unl)
+            write_out(json.loads(unl))
             print("UNL tool running for the", n, "iteration.")
             n+=1
             time.sleep(TIME_PAUSE)
